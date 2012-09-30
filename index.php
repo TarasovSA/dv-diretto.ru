fe85dc3ae5353f7fb0138651ae8b5df2f9733eb5
Sergey Tarasov <tarasovsr@gmail.com>
2012-09-30 18:07:11 +0400

    added support messi popup windows

--- index.php
+++ index.php
@@ -19,8 +19,10 @@ include_once ('engine/auxiliaryFunctions.php');
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" type="text/css" href="/css/themes/ui-lightness/jquery.ui.all.css">
+    <link rel="stylesheet" type="text/css" href="css/messi.min.css">
     <script type="text/javascript" language="javascript" src="js/style_js.js"></script>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+    <script type="text/javascript" language="javascript" src="js/messi.min.js"></script>
     <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.min.js"></script>
     <script type="text/javascript" language="javascript" src="/js/jquery.ui.datepicker-ru.js"></script>
 
