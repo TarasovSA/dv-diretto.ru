2ec32801c8f2e959056d5e6d4405818985764181
Sergey Tarasov <tarasovsr@gmail.com>
2012-10-03 14:11:34 +0400

    implement supporting to editing coefficients

---
+++ admin/blocks/navCoefficient.php
@@ -0,0 +1,9 @@
+<ul class="nav nav-list">
+    <li class="nav-header">Калькуляторы</li>
+    <li class="active"><a href="/admin/index.php?p=coefficient&calc=vilaggio">Villaggio</a></li>
+    <li><a href="/admin/index.php?p=coefficient&calc=feliceCitta">Felice Citta</a></li>
+    <li><a href="/admin/index.php?p=coefficient&calc=bellaVita">Bella Vita</a></li>
+    <li><a href="/admin/index.php?p=coefficient&calc=bellissimo">Bellissimo</a></li>
+    <li class="nav-header">Списки</li>
+    <li><a href="/admin/index.php?p=coefficient&list=auto">Автомобили</a></li>
+</ul>
---
+++ admin/blocks/navMain.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Lamer
+ * Date: 03.10.12
+ * Time: 13:36
+ * To change this template use File | Settings | File Templates.
+ */
