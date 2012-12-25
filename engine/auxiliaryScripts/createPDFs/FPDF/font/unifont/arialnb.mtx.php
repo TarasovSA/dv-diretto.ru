3a947cc1b0c23f1a73fb4db80abb2a06466019b0
Sergey Tarasov <tarasovsr@gmail.com>
2012-12-25 16:32:35 +0400

    added encoding for FPDF

---
+++ engine/auxiliaryScripts/createPDFs/FPDF/font/unifont/arialnb.mtx.php
@@ -0,0 +1,19 @@
+<?php
+$name='ArialNarrow-Bold';
+$type='TTF';
+$desc=array (
+  'Ascent' => 936,
+  'Descent' => -212,
+  'CapHeight' => 936,
+  'Flags' => 262148,
+  'FontBBox' => '[-137 -307 1000 1109]',
+  'ItalicAngle' => 0,
+  'StemV' => 165,
+  'MissingWidth' => 228,
+);
+$up=-106;
+$ut=73;
+$ttffile='/home/paboti/dv-diretto.ru/docs/engine/auxiliaryScripts/createPDFs/FPDF/font/unifont/ARIALNB.TTF';
+$originalsize=178864;
+$fontkey='dejavu';
+?>
